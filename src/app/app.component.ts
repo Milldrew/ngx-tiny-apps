e9b4970e38273a200bd870d4f381270faf923387
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-09 15:22:39 -0700

    📅  | Thu Feb  9 15:22:39 MST 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  recorder: MediaRecorder;
+  audioChunks: Blob[] = [];
+
+  audioContext = new AudioContext();
+  constructor() {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      debugger;
+      const mediaStreamSource =
+        this.audioContext.createMediaStreamSource(stream);
+
+      this.recorder = new MediaRecorder(stream);
+      this.recorder.start();
+
+      this.recorder.addEventListener('dataavailable', (event) => {
+        this.audioChunks.push(event.data);
+        debugger;
+      });
+    });
+  }
+
+  handleStopRecordingButton() {
+    debugger;
+    this.recorder.stop();
+  }
+
+  handlePlayRecordingButton() {
+    debugger;
+    const audioBlob = this.audioChunks[0];
+    const audioUrl = URL.createObjectURL(audioBlob);
+    const audio = new Audio(audioUrl);
+    audio.play();
+  }
+}
