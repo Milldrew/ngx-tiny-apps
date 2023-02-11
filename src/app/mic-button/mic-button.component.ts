7c477a06c5fe979964f4f985ff772b809bdf0953
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-11 12:19:06 -0700

    📅  | Sat Feb 11 12:19:06 MST 2023

--- src/app/mic-button/mic-button.component.ts
+++ src/app/mic-button/mic-button.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'millterial-mic-button',
   templateUrl: './mic-button.component.html',
   styleUrls: ['./mic-button.component.scss'],
 })
-export class MicButtonComponent {}
+export class MicButtonComponent implements OnChanges {
+  @Input()
+  inputVolume: number;
+  isOn = true;
+  handleClick() {
+    this.isOn = !this.isOn;
+  }
+  ngOnChanges() {}
+
+  handleVolumeChange(volumeData: number) {
+    return volumeData;
+  }
+}
