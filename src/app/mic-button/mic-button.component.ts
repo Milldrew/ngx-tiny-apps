9ffec8f7c58ea6d6851be16a1fb06ff3ee398bc3
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-11 13:25:44 -0700

    📅  | Sat Feb 11 13:25:44 MST 2023

--- src/app/mic-button/mic-button.component.ts
+++ src/app/mic-button/mic-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'millterial-mic-button',
@@ -6,6 +7,19 @@ import { Component, Input, OnChanges } from '@angular/core';
   styleUrls: ['./mic-button.component.scss'],
 })
 export class MicButtonComponent implements OnChanges {
+  randomSubject = new Subject<number>();
+  constructor() {
+    setInterval(() => {
+      const randomValue = Math.floor(Math.random() * 11);
+      this.randomSubject.next(randomValue);
+    }, 300);
+    this.randomSubject.subscribe((data) => {
+      console.log(data);
+      this.inputVolume = data;
+      this.setRectangleHeight(data);
+    });
+  }
+
   @Input()
   inputVolume: number;
   isOn = true;
@@ -16,5 +30,10 @@ export class MicButtonComponent implements OnChanges {
 
   handleVolumeChange(volumeData: number) {
     return volumeData;
+  }
+  setRectangleHeight(height: number) {
+    document.body
+      .querySelector('rect')
+      ?.setAttribute('height', height.toString());
   }
 }
