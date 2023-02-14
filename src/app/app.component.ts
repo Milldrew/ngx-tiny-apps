2a4901f505819b28a381f0485917aabdc34ced17
Andrew Miller <andrew.patrick.miller.2022@gmail.com>
2023-02-14 09:51:01 -0700

    📅  | Tue Feb 14 09:51:01 MST 2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,17 +10,38 @@ export class AppComponent {
   analyser: AnalyserNode;
   recorder: MediaRecorder;
   audioChunk: Blob;
+  /**
+   * Ths is used to create the list of recordings.
+   */
+  recordings: Recording[] = [];
   dataArray: Uint8Array;
   audioContext = new AudioContext();
   audioFrequency: number;
   constructor() {}
   handleNewAudioChunk(audioChunk: Blob) {
+    this.recordings.push({
+      timeStamp: new Date().toISOString(),
+      audioChunk,
+      url: this.createUrlForAudio(audioChunk),
+    });
     console.log(audioChunk);
     this.audioChunk = audioChunk;
   }
+  createUrlForAudio(audioChunk: Blob) {
+    return URL.createObjectURL(audioChunk);
+  }
   playAudio() {
-    const audioUrl = URL.createObjectURL(this.audioChunk);
-    const audio = new Audio(audioUrl);
-    audio.play();
+    const audio = new Audio(this.createUrlForAudio(this.audioChunk)).play();
   }
 }
+type Recording = {
+  /**
+   * The time stamp is used to create the list of recordings. and idenitfy the time the recording was created.
+   */
+  timeStamp: string;
+  /**
+   * The audio chunk is the actual audio recording.
+   */
+  audioChunk: Blob;
+  url?: string;
+};
